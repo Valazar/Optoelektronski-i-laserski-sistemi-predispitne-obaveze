bc790f662d590bc02e6b28e634ecda41593c3c0f
valazar <vanjalazarevic5@gmail.com>
2024-03-12 17:00:15 +0100

    commit

---
+++ Display_and_Touch_Screen/Inc/Windows/StopwatchDLG.h
@@ -0,0 +1,52 @@
+
+#include "DIALOG.h"
+
+/*********************************************************************
+*
+*       Defines
+*
+**********************************************************************
+*/
+#define ID_WINDOW_0         (GUI_ID_USER + 0x00)
+#define ID_BUTTON_0         (GUI_ID_USER + 0x01)
+#define ID_BUTTON_1         (GUI_ID_USER + 0x02)
+#define ID_BUTTON_2         (GUI_ID_USER + 0x03)
+#define ID_IMAGE_0         (GUI_ID_USER + 0x04)
+#define ID_BUTTON_3       (GUI_ID_USER + 0x05)
+#define ID_BUTTON_4       (GUI_ID_USER + 0x06)
+#define ID_EDIT_0       (GUI_ID_USER + 0x07)
+#define ID_EDIT_1       (GUI_ID_USER + 0x08)
+#define ID_EDIT_2       (GUI_ID_USER + 0x09)
+#define ID_EDIT_3       (GUI_ID_USER + 0x0A)
+#define ID_EDIT_4       (GUI_ID_USER + 0x0B)
+#define ID_EDIT_5       (GUI_ID_USER + 0x0C)
+#define ID_EDIT_6       (GUI_ID_USER + 0x0D)
+#define ID_EDIT_7       (GUI_ID_USER + 0x0E)
+#define ID_EDIT_8       (GUI_ID_USER + 0x0F)
+#define ID_EDIT_9       (GUI_ID_USER + 0x10)
+#define ID_EDIT_10       (GUI_ID_USER + 0x11)
+#define ID_EDIT_11       (GUI_ID_USER + 0x12)
+#define ID_EDIT_12       (GUI_ID_USER + 0x13)
+#define ID_BUTTON_5       (GUI_ID_USER + 0x14)
+#define ID_BUTTON_6       (GUI_ID_USER + 0x15)
+#define ID_BUTTON_7       (GUI_ID_USER + 0x16)
+#define ID_BUTTON_8       (GUI_ID_USER + 0x17)
+#define ID_BUTTON_9       (GUI_ID_USER + 0x18)
+#define ID_EDIT_13       (GUI_ID_USER + 0x19)
+#define ID_EDIT_14       (GUI_ID_USER + 0x1A)
+#define ID_EDIT_15       (GUI_ID_USER + 0x1B)
+#define ID_EDIT_16       (GUI_ID_USER + 0x1C)
+#define ID_EDIT_17       (GUI_ID_USER + 0x1D)
+#define ID_EDIT_18       (GUI_ID_USER + 0x1E)
+#define ID_TEXT_0       (GUI_ID_USER + 0x1F)
+#define ID_TEXT_1       (GUI_ID_USER + 0x20)
+#define ID_TEXT_2       (GUI_ID_USER + 0x21)
+#define ID_TEXT_3       (GUI_ID_USER + 0x22)
+#define ID_BUTTON_10       (GUI_ID_USER + 0x23)
+#define ID_BUTTON_11       (GUI_ID_USER + 0x24)
+#define ID_BUTTON_12       (GUI_ID_USER + 0x25)
+#define ID_BUTTON_13       (GUI_ID_USER + 0x26)
+
+#define ID_IMAGE_0_IMAGE_0       0x00
+
+WM_HWIN CreateStopwatch(void);
